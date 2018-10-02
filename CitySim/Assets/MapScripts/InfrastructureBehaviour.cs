4a404e058e4b640c47c7ed7a4d94c25896de7163
Troy Rubenstein <tbr29@scarletmail.rutgers.edu>
2018-10-01 23:48:00 -0400

    Path Made

---
+++ CitySim/Assets/MapScripts/InfrastructureBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MapReader))]
+abstract class InfrastructureBehaviour : MonoBehaviour
+{
+    protected MapReader map;
+
+    void Awake()
+    {
+        map = GetComponent<MapReader>();
+    }
+
+    protected Vector3 GetCenter(OSMWay way)
+    {
+        Vector3 total = Vector3.zero;
+
+        foreach (var id in way.NodeIDs)
+        {
+            total += map.nodes[id];
+        }
+
+        return total / way.NodeIDs.Count;
+    }
+}
---
+++ CitySim/Assets/MovingScripts/CarMove.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarMove : MonoBehaviour {
+
+    //public Vector3 X = new Vector3();
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent component not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+	}
+
+    private void SetDestination()
+    {
+        Debug.Log(_destination);
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
